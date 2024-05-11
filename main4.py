cf2472c51b069701a7888af49f9d3939cf6f92c4
Akezhan <akezhanshg97@gmail.com>
2024-05-11 10:22:00 +0500

    Add files via upload

---
+++ main4.py
@@ -0,0 +1,80 @@
+import telebot
+from telebot import types
+import openpyxl
+
+bot = telebot.TeleBot("7119147941:AAHB4AuZZQygzTd69mmdtlnEPP-KUlaLh4Y")
+
+user_data = {}
+
+
+# to save anything in excelfile
+def save_data(fio, phone, ticket_number, user_id):
+    wb = openpyxl.load_workbook('data.xlsx')
+    ws = wb.active
+    ws.append([fio, phone, ticket_number, user_id])
+    wb.save('data.xlsx')
+
+
+# handler for start
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Сәлем! Аты-жөніңізді енгізіңіз:")
+    user_data[message.chat.id] = {}
+    bot.register_next_step_handler(message, input_fio)
+
+
+# Обработчик для ввода ФИО
+def input_fio(message):
+    user_data[message.chat.id]['fio'] = message.text
+    bot.send_message(message.chat.id, "Введите номер телефона:")
+    bot.register_next_step_handler(message, input_phone)
+
+
+# handler phone
+def input_phone(message):
+    user_data[message.chat.id]['phone'] = message.text
+    bot.send_message(message.chat.id, "Билет номерін енгізіңіз:")
+    bot.register_next_step_handler(message, input_ticket_number)
+
+
+# handler ticket number
+def input_ticket_number(message):
+    if ticket_number_exists(message.text):
+        bot.send_message(message.chat.id, "Бұл билет тіркелген, басқа номер енгізіңіз::")
+        bot.register_next_step_handler(message, input_ticket_number)
+    else:
+        user_data[message.chat.id]['ticket_number'] = message.text
+        bot.send_message(message.chat.id, "Билеттің суретін жіберіңіз:")
+        bot.register_next_step_handler(message, input_ticket_photo)
+
+
+# ticket image handler
+def input_ticket_photo(message):
+    user_id = message.from_user.id
+    ticket_number = user_data[message.chat.id]['ticket_number']
+    if message.photo:
+        file_id = message.photo[-1].file_id
+        file_info = bot.get_file(file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+        file_path = "photos/{}.jpg".format(ticket_number)
+        with open(file_path, 'wb') as new_file:
+            new_file.write(downloaded_file)
+        save_data(user_data[message.chat.id]['fio'], user_data[message.chat.id]['phone'], ticket_number, user_id)
+        bot.send_message(message.chat.id, "Құттықтаймын! Сіздің билетіңіз тіркелді!♥")
+        bot.send_message(message.chat.id, "Жаңа билет тіркеу үшін /start батырмасын басыңыз.")
+    else:
+        bot.send_message(message.chat.id, "Сурет жіберіңіз өтініш!")
+        bot.register_next_step_handler(message, input_ticket_photo)
+
+
+def ticket_number_exists(ticket_number):
+    wb = openpyxl.load_workbook('data.xlsx')
+    ws = wb.active
+    for row in ws.iter_rows(values_only=True):
+        if row[2] == ticket_number:
+            return True
+    return False
+
+
+# run
+bot.polling()
